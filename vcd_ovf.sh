d2df98b8f6d9ed8ff2db729ca022766fa95693bf
danpaul81 <dp@danielpaul.de>
2022-03-14 11:33:17 +0100

    optimize vcd deployment script

--- vcd_ovf.sh
+++ vcd_ovf.sh
@@ -1,45 +1,38 @@
 #!/bin/bash
 
-SESSIONID=6da64e4ea7284b89aec6d38840822144
+#session ID on VCD
+SESSIONID=05055206297f4c0d91cb5a702494307d
 
-#ovftool --noSSLVerify --I:targetSessionTicket=$SESSIONID --diskMode=thin  \
---prop:guestinfo.master_ip='192.168.110.210' \
---prop:guestinfo.node_ip='192.168.110.211' \
---prop:guestinfo.netmask='24' \
---prop:guestinfo.gateway='192.168.110.1' \
---prop:guestinfo.dns='192.168.110.10' \
---prop:guestinfo.domain='corp.local' \
---prop:guestinfo.ntp='192.168.110.10' \
---prop:guestinfo.root_password='VMware1!VMware1!' \
---prop:guestinfo.debug=False \
---prop:guestinfo.preload=False \
---prop:guestinfo.nappautodeploy=False \
---prop:guestinfo.vip_pool='192.168.110.200-192.168.110.205' \
---prop:guestinfo.nappfqdn='napp.corp.local' \
---prop:guestinfo.nsxmanager='192.168.110.201' \
---prop:guestinfo.nsxuser='admin' \
---prop:guestinfo.nsxpassword='VMware1!VMware1!' \
---prop:guestinfo.podnet='172.25.0.0/16' \
---prop:guestinfo.role='master' \
-output-vsphere-iso/NAPP_Appliance_0.0.2_master_app.ova 'vcloud://ams2-vcd01.oc.vmware.com/cloud?org=ams2-vcd01-emea-sddc-lt-labs-t&vdc=ams2-vcd01-emea-sddc-lt-labs-d-ovdc1&vapp=NAPP_MASTER'
+#local path to ova sources
+SOURCE_MASTER='output-vsphere-iso/NAPP_Appliance_0.0.2_master_app.ova'
+SOURCE_NODE='output-vsphere-iso/NAPP_Appliance_0.0.2_node_app.ova'
 
-ovftool --noSSLVerify --I:targetSessionTicket=$SESSIONID --diskMode=thin  \
---prop:guestinfo.master_ip='192.168.110.210' \
---prop:guestinfo.node_ip='192.168.110.211' \
---prop:guestinfo.netmask='24' \
---prop:guestinfo.gateway='192.168.110.1' \
---prop:guestinfo.dns='192.168.110.10' \
---prop:guestinfo.domain='corp.local' \
---prop:guestinfo.ntp='192.168.110.10' \
---prop:guestinfo.root_password='VMware1!VMware1!' \
---prop:guestinfo.debug=False \
---prop:guestinfo.preload=False \
---prop:guestinfo.nappautodeploy=False \
---prop:guestinfo.vip_pool='192.168.110.200-192.168.110.205' \
---prop:guestinfo.nappfqdn='napp.corp.local' \
---prop:guestinfo.nsxmanager='192.168.110.201' \
---prop:guestinfo.nsxuser='admin' \
---prop:guestinfo.nsxpassword='VMware1!VMware1!' \
---prop:guestinfo.podnet='172.25.0.0/16' \
---prop:guestinfo.role='node' \
-output-vsphere-iso/NAPP_Appliance_0.0.2_node_app.ova 'vcloud://ams2-vcd01.oc.vmware.com/cloud?org=ams2-vcd01-emea-sddc-lt-labs-t&vdc=ams2-vcd01-emea-sddc-lt-labs-d-ovdc1&vapp=NAPP_NODE'
+#VCD URL/org/vdc/vapp
+#important: select different VAPP names for master/node
+TARGET_MASTER='vcloud://ams2-vcd01.oc.vmware.com/cloud?org=ams2-vcd01-emea-sddc-lt-labs-t&vdc=ams2-vcd01-emea-sddc-lt-labs-d-ovdc1&vapp=NAPP_MASTER'
+TARGET_NODE='vcloud://ams2-vcd01.oc.vmware.com/cloud?org=ams2-vcd01-emea-sddc-lt-labs-t&vdc=ams2-vcd01-emea-sddc-lt-labs-d-ovdc1&vapp=NAPP_NODE'
+
+#
+#review and corect. dont use simple root password as deployment might fail
+COMMONOPTS="--noSSLVerify --diskMode=thin 
+--prop:guestinfo.master_ip='192.168.110.210'  
+--prop:guestinfo.node_ip='192.168.110.211'  
+--prop:guestinfo.netmask='24' 
+--prop:guestinfo.gateway='192.168.110.1'  
+--prop:guestinfo.dns='192.168.110.10'
+--prop:guestinfo.domain='corp.local' 
+--prop:guestinfo.ntp='192.168.110.10'
+--prop:guestinfo.root_password='VMware1!VMware1!' 
+--prop:guestinfo.debug=False 
+--prop:guestinfo.preload=True 
+--prop:guestinfo.nappautodeploy=True  
+--prop:guestinfo.vip_pool='192.168.110.215-192.168.110.220'  
+--prop:guestinfo.nappfqdn='napp.corp.local' 
+--prop:guestinfo.nsxmanager='192.168.110.201'  
+--prop:guestinfo.nsxuser='admin' 
+--prop:guestinfo.nsxpassword='VMware1!VMware1!'  
+--prop:guestinfo.podnet='172.25.0.0/16' "
+
+
+ovftool --I:targetSessionTicket=$SESSIONID $COMMONOPTS --prop:guestinfo.role='master' $SOURCE_MASTER $TARGET_MASTER
+ovftool --I:targetSessionTicket=$SESSIONID $COMMONOPTS --prop:guestinfo.role='node'   $SOURCE_NODE   $TARGET_NODE
