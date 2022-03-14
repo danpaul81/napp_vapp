21289edb287ad3b5e4fe4813c33a50b664d2e433
danpaul81 <dp@danielpaul.de>
2022-03-14 12:12:50 +0100

    vcd script

--- vcd_ovf.sh
+++ vcd_ovf.sh
@@ -14,25 +14,25 @@ TARGET_NODE='vcloud://ams2-vcd01.oc.vmware.com/cloud?org=ams2-vcd01-emea-sddc-lt
 
 #
 #review and corect. dont use simple root password as deployment might fail
-COMMONOPTS="--noSSLVerify --diskMode=thin 
---prop:guestinfo.master_ip='192.168.110.210'  
---prop:guestinfo.node_ip='192.168.110.211'  
---prop:guestinfo.netmask='24' 
---prop:guestinfo.gateway='192.168.110.1'  
---prop:guestinfo.dns='192.168.110.10'
---prop:guestinfo.domain='corp.local' 
---prop:guestinfo.ntp='192.168.110.10'
---prop:guestinfo.root_password='VMware1!VMware1!' 
+COMMONOPTS='--noSSLVerify --diskMode=thin 
+--prop:guestinfo.master_ip=192.168.110.210  
+--prop:guestinfo.node_ip=192.168.110.211
+--prop:guestinfo.netmask=24 
+--prop:guestinfo.gateway=192.168.110.1  
+--prop:guestinfo.dns=192.168.110.10
+--prop:guestinfo.domain=corp.local 
+--prop:guestinfo.ntp=192.168.110.10
+--prop:guestinfo.root_password=VMware1!VMware1! 
 --prop:guestinfo.debug=False 
 --prop:guestinfo.preload=True 
 --prop:guestinfo.nappautodeploy=True  
---prop:guestinfo.vip_pool='192.168.110.215-192.168.110.220'  
---prop:guestinfo.nappfqdn='napp.corp.local' 
---prop:guestinfo.nsxmanager='192.168.110.201'  
---prop:guestinfo.nsxuser='admin' 
---prop:guestinfo.nsxpassword='VMware1!VMware1!'  
---prop:guestinfo.podnet='172.25.0.0/16' "
+--prop:guestinfo.vip_pool=192.168.110.215-192.168.110.220  
+--prop:guestinfo.nappfqdn=napp.corp.local
+--prop:guestinfo.nsxmanager=192.168.110.201  
+--prop:guestinfo.nsxuser=admin
+--prop:guestinfo.nsxpassword=VMware1!VMware1!  
+--prop:guestinfo.podnet=172.25.0.0/16'
 
-
-ovftool --I:targetSessionTicket=$SESSIONID $COMMONOPTS --prop:guestinfo.role='master' $SOURCE_MASTER $TARGET_MASTER
-ovftool --I:targetSessionTicket=$SESSIONID $COMMONOPTS --prop:guestinfo.role='node'   $SOURCE_NODE   $TARGET_NODE
+#echo $COMMONOPTS
+ovftool --I:targetSessionTicket=$SESSIONID $COMMONOPTS --prop:guestinfo.role=master $SOURCE_MASTER $TARGET_MASTER
+ovftool --I:targetSessionTicket=$SESSIONID $COMMONOPTS --prop:guestinfo.role=node   $SOURCE_NODE   $TARGET_NODE
